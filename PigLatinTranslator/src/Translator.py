faf6bfa1d0ca154e7fc5e1eb222a2a25371bed1b
Tristan Jenkins <Tris1>
2013-10-29 10:17:12 -0400

    A little pig latin translator that I created. The program runs in python but currently not in PyDev for Eclipse IDE. I'm working on a fix.

---
+++ PigLatinTranslator/src/Translator.py
@@ -0,0 +1,22 @@
+'''
+Created on Oct 29, 2013
+
+@author: Tristan
+Simply translates user input into correct Pig Latin. Have fun
+'''
+
+pyg = 'ay'
+
+original = raw_input('Enter a word:')
+
+if len(original) > 0 and original.isalpha():
+    word = original.lower()
+    first = word[0]
+    if first == ('a' or 'e' or 'i' or 'u' or 'o'):
+        new_word = word + pyg
+        print new_word
+    else:
+        new_word = word[1:] + first + pyg
+        print new_word
+else:
+    print 'We cannot translate your input! Please try again.'
