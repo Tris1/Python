8dde85726c960f06e52a68b74a980fd5aa707cd4
Tristan Jenkins <Tris1>
2013-10-29 10:20:43 -0400

    Fixed the program so that it runs correctly in Eclipse under PyDev.

--- PigLatinTranslator/src/Translator.py
+++ PigLatinTranslator/src/Translator.py
@@ -7,16 +7,16 @@ Simply translates user input into correct Pig Latin. Have fun
 
 pyg = 'ay'
 
-original = raw_input('Enter a word:')
+original = input('Enter a word:')
 
 if len(original) > 0 and original.isalpha():
     word = original.lower()
     first = word[0]
     if first == ('a' or 'e' or 'i' or 'u' or 'o'):
         new_word = word + pyg
-        print new_word
+        print (new_word)
     else:
         new_word = word[1:] + first + pyg
-        print new_word
+        print (new_word)
 else:
-    print 'We cannot translate your input! Please try again.'
+    print ('We cannot translate your input! Please try again.')
