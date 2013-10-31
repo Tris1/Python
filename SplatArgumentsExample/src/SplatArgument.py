1f9136bc34c35bd371dfa4d09c281214bb8c6c41
Tristan Jenkins <Tris1>
2013-10-30 20:08:41 -0400

    Added a check to ensure that a user has at least Python Version 3.x on their system.

--- SplatArgumentsExample/src/SplatArgument.py
+++ SplatArgumentsExample/src/SplatArgument.py
@@ -6,9 +6,21 @@ Here is an example on how to use Splat arguments in Python. Splat arguments tell
 unsure of how many arguments will be passed into a function, but it will most likely be more than one.
 '''
 
+#Check python version
+import sys
+python2 = False
+
+if sys.version_info[:2] <= (3, 0):
+    python2 = True
+    
+if python2 == True:
+    print ("Please download SplatArgumentV2.py to run on your system.")
+    sys.exit(2)
+
 """To use splat arguments, type a '*' before the input parameters, in this case, 'actors'.
 When you call on the parameters, you only need the name of the parameter that you declared,
 not the '*' """
+
 def favorite_actors(*actors):
     """Prints out your favorite actorS (plural!)"""
     print ("Your favorite actors are:",  actors)
