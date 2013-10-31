c0740bc9710ceb3583091c7c70dd31183e203df6
Tristan Jenkins <Tris1>
2013-10-30 20:22:11 -0400

    SplatAsExponent.py file designed to run on Version 2.x

---
+++ SplatArgumentsExample/src/SplatAsExponentV2.py
@@ -0,0 +1,13 @@
+'''
+Created on Oct 30, 2013
+
+@author: Tristan
+Here is an example on how to use Splat arguments in Python Version 2.x. One can also use the 'splat' operator to perform exponentiation.
+'''
+def powerRiser(base, exponent):
+    #We use the splat command in the following form: base ** exponent
+    print base ** exponent
+
+powerRiser(2,3)
+powerRiser(1,2)
+powerRiser(16,4)
