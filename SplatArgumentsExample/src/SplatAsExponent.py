12830f56fdbe8b892eef388773c149d1941b455c
Tristan Jenkins <Tris1>
2013-10-30 20:21:08 -0400

    Added python version checker.

--- SplatArgumentsExample/src/SplatAsExponent.py
+++ SplatArgumentsExample/src/SplatAsExponent.py
@@ -4,6 +4,17 @@ Created on Oct 30, 2013
 @author: Tristan
 Here is an example on how to use Splat arguments in Python. One can also use the 'splat' operator to perform exponentiation.
 '''
+#Check python version
+import sys
+python2 = False
+
+if sys.version_info[:2] <= (3, 0):
+    python2 = True
+    
+if python2 == True:
+    print ("Please download SplatAsExponentV2.py to run this program on your system.")
+    sys.exit(2)
+
 def powerRiser(base, exponent):
     #We use the splat command in the following form: base ** exponent
     print (base ** exponent)
